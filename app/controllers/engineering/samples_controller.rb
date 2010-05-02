c5b72470b9ccf6a4205214074aa0233240807ed3
David Kirkby <dkirkby@uci.edu>
2010-05-02 12:54:15 -0700

    Implement recent samples action and view

--- app/controllers/engineering/samples_controller.rb
+++ app/controllers/engineering/samples_controller.rb
@@ -1,5 +1,9 @@
 class Engineering::SamplesController < ApplicationController
+
   def recent
+    @nrecent = params['n']
+    @count = Sample.count
+    @samples = Sample.find(:all,:limit=>@nrecent,:order=>'created_at DESC',:readonly=>true)
   end
 
   def bydate
