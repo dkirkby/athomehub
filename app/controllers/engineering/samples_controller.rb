c51668190b0a42f8243a04556db25aa6046a8b29
David Kirkby <dkirkby@uci.edu>
2010-05-02 13:18:05 -0700

    Implement range checking on param n in recent action

--- app/controllers/engineering/samples_controller.rb
+++ app/controllers/engineering/samples_controller.rb
@@ -1,12 +1,28 @@
 class Engineering::SamplesController < ApplicationController
 
+  before_filter :valid_n,:only=>:recent
+
   def recent
-    @nrecent = params['n']
     @count = Sample.count
     @samples = Sample.find(:all,:limit=>@nrecent,:order=>'created_at DESC',:readonly=>true)
   end
 
   def bydate
   end
-
+  
+protected
+  
+  # Validates params['n'] and uses it to set @nrecent
+  def valid_n
+    @nrecent = 10 # this is the default
+    if params.has_key? 'n'
+      value = params['n'].to_i
+      if value < 1 or value > 1000
+        flash[:notice] = "Invalid parameter n=#{params['n']}. Using #{@nrecent} instead."
+      else
+        @nrecent = value
+      end
+    end
+  end
+  
 end
