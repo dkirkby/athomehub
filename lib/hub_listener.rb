f1652342e66a59928c41df192a17249d418c9237
David Kirkby <dkirkby@uci.edu>
2010-05-18 07:50:00 -0700

    add some method doc comments

--- lib/hub_listener.rb
+++ lib/hub_listener.rb
@@ -4,6 +4,8 @@ class HubListener
   
   include Singleton
   
+  # Looks for a running listener process and sets the @pid global if one
+  # is found.
   def initialize
     # look for a file containing the PID of the running listener process
     @pidFile = "/tmp/hub_listener.pid"
@@ -13,34 +15,56 @@ class HubListener
       begin
         Process.getpriority(Process::PRIO_PROCESS,@pid)
       rescue
-        cleanup
+        self.cleanup
       end
     end
   end
   
+  # Returns a status string that reports wether a listener is running and
+  # what serial port a hub is connected to.
   def status
-    if @pid then
-      "hub listener running as PID #{@pid}"
+    # do we have a hub connected to a serial port?
+    if self.port then
+      port_status = " (using serial port #{self.port})"
     else
-      "no hub listener is running"
+      port_status = " (no hub serial port found)"
+    end
+    # is there a listener process running?
+    if @pid then
+      "hub listener running as PID #{@pid}" + port_status
+    else
+      "no hub listener is running" + port_status
     end
   end
   
+  # Starts a new hub listener in a forked subprocess.
   def start
     raise "hub listener already running as PID #{@pid}" if @pid
   end
 
+  # Stops a running hub listener, which may have been started in another
+  # ruby instance.
   def stop
     raise "no hub listener process is running" unless @pid
     Process.kill("INT",@pid)
-    cleanup
+    self.cleanup
   end
   
-  private
+  protected
   
+  # Cleans up any old PID file and removes any record of a running process
   def cleanup
     File.delete(@pidFile) if File.exists? @pidFile
     @pid = nil
+  end
+
+  # Returns the name of the serial port device that we think a hub is connected to.
+  def port
+    (Dir.glob("/dev/tty.usbserial-*") | Dir.glob("/dev/ttyusb*")).first
+  end
+  
+  # Connects to a hub serial port and enters an infinite message handling loop.
+  def listen
   end
 
 end
