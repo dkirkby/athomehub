73d7c8c797ce83f2f69e4890d3bfff3b58712b5c
David Kirkby <dkirkby@uci.edu>
2010-05-20 15:11:06 -0700

    add more config validations

--- app/models/device_config.rb
+++ app/models/device_config.rb
@@ -4,4 +4,14 @@ class DeviceConfig < ActiveRecord::Base
   validates_uniqueness_of :networkID
   validates_uniqueness_of :location
 
+  validates_numericality_of :minTemperature,
+    :greater_than_or_equal_to=>0, :less_than=>200
+  validates_numericality_of :maxTemperature,
+    :greater_than_or_equal_to=>0, :less_than=>200
+  validates_numericality_of :networkID, :only_integer=>true,
+    :greater_than_or_equal_to=>0, :less_than=>255
+    
+  validates_format_of :serialNumber, :with=>/^[0-9a-fA-F]{8}$/,
+    :message=>"is invalid (expected 8 hex digits)"
+
 end
