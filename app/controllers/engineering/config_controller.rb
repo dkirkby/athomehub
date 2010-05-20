bc9cd476b44b1c3afd3c9149c6748a161fd11401
David Kirkby <dkirkby@uci.edu>
2010-05-20 09:26:24 -0700

    flesh out config controller and index view

--- app/controllers/engineering/config_controller.rb
+++ app/controllers/engineering/config_controller.rb
@@ -1,13 +1,41 @@
 class Engineering::ConfigController < ApplicationController
 
   def index
-    @configs = DeviceConfigs.find(:all)
+    @configs = DeviceConfig.all
+  end
+
+  def new
+    @config = DeviceConfig.new    
+  end
+  
+  def edit
+    @config = DeviceConfig.find(params[:id])
   end
 
   def create
+    @config = DeviceConfig.new(params[:config])
+    if @config.save
+      flash[:notice] = 'DeviceConfig was successfully created.'
+      redirect_to @config
+    else
+      render :action => "new"
+    end
   end
 
   def update
+    @config = DeviceConfig.find(params[:id])
+    if @config.update_attributes(params[:config])
+      flash[:notice] = 'DeviceConfig was successfully updated.'
+      redirect_to @config
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @config = DeviceConfig.find(params[:id])
+    @config.destroy
+    redirect_to engineering_configs_url
   end
 
 end
