728ea17b42da5deb5a7bdf030c2d6330aa787109
David Kirkby <dkirkby@uci.edu>
2010-05-21 12:15:49 -0700

    replicate lam controller actions for log controller

--- app/controllers/engineering/log_controller.rb
+++ app/controllers/engineering/log_controller.rb
@@ -1,8 +1,17 @@
-class Engineering::LogController < ApplicationController
+class Engineering::LogController < Engineering::ApplicationController
+
+  before_filter :valid_n,:only=>:recent
+  before_filter :valid_ival,:only=>:bydate
+
   def recent
+    @count = LookAtMe.count
+    @logs = LookAtMe.find(:all,:limit=>@n,:order=>'id DESC',:readonly=>true)
   end
 
   def bydate
+    @logs = LookAtMe.find(:all,
+      :conditions=>['created_at > ? and created_at <= ?',@begin_at,@end_at],
+      :order=>'created_at DESC',:readonly=>true)
   end
 
 end
