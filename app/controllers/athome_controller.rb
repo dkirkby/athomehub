966482b9a89585a76a20a6b2ba751fff6c24587e
David Kirkby <dkirkby@uci.edu>
2010-05-22 09:12:09 -0700

    add demo action to display 8 devices with dummy data

--- app/controllers/athome_controller.rb
+++ app/controllers/athome_controller.rb
@@ -1,8 +1,57 @@
+# Define a replacement Sample class
+class DemoSample
+  def initialize(fields)
+    @fields = fields
+  end
+  def temperature
+    return @fields[:temperature]
+  end
+  def lighting
+    return @fields[:lighting]
+  end
+  def artificial
+    return @fields[:artificial]
+  end
+  def power
+    return @fields[:power]
+  end
+  def cost
+    return @fields[:cost]
+  end
+  def location
+    return @fields[:location]
+  end
+end
+
 class AthomeController < ApplicationController
 
   def index
     @samples = Sample.find(:all,:group=>'networkID',
       :conditions=>'networkID IS NOT NULL',:readonly=>true)
+  end
+  
+  # Returns fake data for demonstration and UI prototyping
+  def demo
+    # Declare some sample data
+    @samples = [
+      DemoSample.new({:location=>"Kitchen",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Family Room",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Dylan's Room",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Downstairs Bathroom",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Kids Bathroom",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Helen's Room",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Master Bedroom",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731}),
+      DemoSample.new({:location=>"Master Bathroom",
+        :temperature=>7241,:lighting=>74,:artificial=>43,:power=>1328,:cost=>0.731})
+    ]
+    render :action=>"index"
   end
 
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,7 +1,13 @@
 class Sample < ActiveRecord::Base
+
   belongs_to :config,
     :class_name => "DeviceConfig",
     :foreign_key => "networkID",
     :primary_key => "networkID",
     :readonly => true
+
+  def location
+    self.config.location
+  end
+
 end
