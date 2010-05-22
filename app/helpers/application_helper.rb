55e264bb69002a48f8588da41d91fe3c7f300f91
David Kirkby <dkirkby@uci.edu>
2010-05-22 15:15:09 -0700

    use application helpers to format date & time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,19 @@ module ApplicationHelper
   def format_serialNumber(sn)
     "#{sn[0,2]}-#{sn[2,2]}-#{sn[4,2]}-#{sn[6,2]}"
   end
+  
+  # Formats the provided UTC Time object and returns a sanitized date string.
+  def format_date(timestamp)
+    h timestamp.localtime.strftime("%A %d %B")
+  end
+  
+  # Formats the provided UTC Time object and returns a sanitized time string.
+  def format_time(timestamp)
+    formatted = h timestamp.localtime.strftime("%I:%M%p")
+    # drop a leading zero on the hour
+    formatted.slice! /^0/
+    # replace AM/PM with am/pm
+    formatted.downcase
+  end
 
 end
