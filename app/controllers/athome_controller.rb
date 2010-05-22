d56a19964b8052f66890e4c1cd0244163141e13f
David Kirkby <dkirkby@uci.edu>
2010-05-22 08:02:54 -0700

    display some numbers

--- app/controllers/athome_controller.rb
+++ app/controllers/athome_controller.rb
@@ -1,6 +1,8 @@
 class AthomeController < ApplicationController
 
   def index
+    @samples = Sample.find(:all,:group=>'networkID',
+      :conditions=>'networkID IS NOT NULL',:readonly=>true)
   end
 
 end
