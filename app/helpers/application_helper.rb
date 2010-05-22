c323ddc42b46e34fe5dc9278d55c9da8f1773342
David Kirkby <dkirkby@uci.edu>
2010-05-21 17:49:38 -0700

    update config index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def format_serialNumber(sn)
+    "#{sn[0,2]}-#{sn[2,2]}-#{sn[4,2]}-#{sn[6,2]}"
+  end
+
 end
--- app/helpers/engineering/lam_helper.rb
+++ app/helpers/engineering/lam_helper.rb
@@ -1,10 +1,5 @@
 module Engineering::LamHelper
   
-  def format_serialNumber(lam)
-    s = lam.serialNumber
-    "#{s[0,2]}-#{s[2,2]}-#{s[4,2]}-#{s[6,2]}"
-  end
-  
   def link_to_commitID(lam)
     link_to lam.commitID,"http://github.com/dkirkby/athomeleaf/commit/#{lam.commitID}" 
   end
