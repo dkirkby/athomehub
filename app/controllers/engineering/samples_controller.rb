0861c4077c8cc82f2abe15ef6ad931841e801640
David Kirkby <dkirkby@uci.edu>
2010-05-21 17:13:34 -0700

    update engineering samples views

--- app/controllers/engineering/samples_controller.rb
+++ app/controllers/engineering/samples_controller.rb
@@ -1,13 +1,17 @@
-class Engineering::SamplesController < ApplicationController
+class Engineering::SamplesController < Engineering::ApplicationController
 
   before_filter :valid_n,:only=>:recent
+  before_filter :valid_ival,:only=>:bydate
 
   def recent
     @count = Sample.count
-    @samples = Sample.find(:all,:limit=>@nrecent,:order=>'id DESC',:readonly=>true)
+    @samples = Sample.find(:all,:limit=>@n,:order=>'id DESC',:readonly=>true)
   end
 
   def bydate
+    @samples = Sample.find(:all,
+      :conditions=>['created_at > ? and created_at <= ?',@begin_at,@end_at],
+      :order=>'created_at DESC',:readonly=>true)
   end
   
 protected
