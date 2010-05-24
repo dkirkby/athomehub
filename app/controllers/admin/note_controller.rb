b7970d89afc24acb35bbfdaf183aeb8020adb1db
David Kirkby <dkirkby@uci.edu>
2010-05-23 20:58:00 -0700

    replace admin note index action with recent, bydate actions

--- app/controllers/admin/note_controller.rb
+++ app/controllers/admin/note_controller.rb
@@ -1,7 +1,17 @@
 class Admin::NoteController < Admin::ApplicationController
 
-  def index
-    @notes = Note.all
+  before_filter :valid_n,:only=>:recent
+  before_filter :valid_ival,:only=>:bydate
+
+  def recent
+    @count = Note.count
+    @notes = Note.find(:all,:limit=>@n,:order=>'id DESC',:readonly=>true)
+  end
+
+  def bydate
+    @notes = Note.find(:all,
+      :conditions=>['created_at > ? and created_at <= ?',@begin_at,@end_at],
+      :order=>'created_at DESC',:readonly=>true)
   end
 
 end
