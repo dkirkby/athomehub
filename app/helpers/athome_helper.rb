9a136fbc126cadcfece7cd3aaae3232e1ecb0c90
David Kirkby <dkirkby@uci.edu>
2010-05-23 17:10:22 -0700

    move complex remote link stuff to a helper

--- app/helpers/athome_helper.rb
+++ app/helpers/athome_helper.rb
@@ -1,2 +1,12 @@
 module AthomeHelper
+  
+  # Inserts a link that, when clicked, creates a new note by the specified
+  # user via an ajax call, and resets the note_body textarea contents.
+  def note_for_user_link(link_text,user)
+    link_to_remote link_text,
+      :url=>{:action=>"create_note",:user_id=>user.id},
+      :submit=>"note",
+      :complete=>"$('note_body').setValue('Note saved. Click here to enter another one...');"
+  end
+
 end
