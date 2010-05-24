3fde2c0bc48e9b210171de1259a89cdbc4f08569
David Kirkby <dkirkby@uci.edu>
2010-05-24 13:11:41 -0700

    bydate.txt returns raw sample data

--- app/controllers/engineering/samples_controller.rb
+++ app/controllers/engineering/samples_controller.rb
@@ -12,6 +12,10 @@ class Engineering::SamplesController < Engineering::ApplicationController
     @samples = Sample.find(:all,
       :conditions=>['created_at > ? and created_at <= ?',@begin_at,@end_at],
       :order=>'created_at DESC',:readonly=>true)
+    respond_to do |format|
+      format.html
+      format.text { render :text=> dump_by_device }
+    end
   end
   
   def last
@@ -20,5 +24,31 @@ class Engineering::SamplesController < Engineering::ApplicationController
   end
   
 protected
+  
+  def dump_by_device
+    # initialize the data dump we will return
+    dump = [ ]
+    # loop over @samples, building separate arrays for each device
+    by_device = { }
+    @samples.each do |s|
+      # create a new empty array when we first see a device
+      if not by_device.has_key? s.networkID then
+        by_device[s.networkID] = [ ]
+      end
+      # append this sample
+      by_device[s.networkID] << s
+    end
+    # dump the data by device
+    by_device.each do |id,data|
+      dump << "# ID#{id}"
+      data.each do |s|
+        dump << sprintf("%10d %6d %6d %6d %6d %7.2f",s.created_at.to_i,
+          s.lighting,s.artificial,s.lighting2,s.artificial2,1e-2*s.temperature)
+      end
+      # insert two blank lines between devices
+      dump << "" << ""
+    end
+    dump.join "\n"
+  end
   
 end
