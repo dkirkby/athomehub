64d0e183893590f9352157e560203789cf3739a2
David Kirkby <dkirkby@uci.edu>
2010-06-03 12:48:29 -0700

    declare dump -> config dependency

--- app/models/buffer_dump.rb
+++ app/models/buffer_dump.rb
@@ -1,2 +1,9 @@
 class BufferDump < ActiveRecord::Base
+
+  belongs_to :config,
+    :class_name => "DeviceConfig",
+    :foreign_key => "networkID",
+    :primary_key => "networkID",
+    :readonly => true
+
 end
