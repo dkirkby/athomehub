3e9cd8ecaa94f849e55da707def7676a08a3dadf
David Kirkby <dkirkby@uci.edu>
2010-06-03 16:12:03 -0700

    dump view display symbolic source name

--- app/helpers/engineering/dump_helper.rb
+++ app/helpers/engineering/dump_helper.rb
@@ -1,2 +1,8 @@
 module Engineering::DumpHelper
+  
+  def source_label(dump)
+    return 'UNKNOWN' unless (0..4).include? dump.source
+    ['PowerLO','PowerHI','LightLO','LightHI','ACPhase'][dump.source]
+  end
+  
 end
