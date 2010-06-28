18a341c589d1cc490f047b2be1ee921cc27c2883
David Kirkby <dkirkby@uci.edu>
2010-06-27 21:32:26 -0700

    display model for phase analysis

--- app/controllers/engineering/dump_controller.rb
+++ app/controllers/engineering/dump_controller.rb
@@ -18,17 +18,16 @@ class Engineering::DumpController < Engineering::ApplicationController
     @dump= BufferDump.find(params['id'])
     # fill a 2 x 250 table of (time,adc) values
     @data = Array.new 250
-    @model = Array.new 250
     @dump.samples.each_index do |k|
       @data[k] = [ 200*k, @dump.samples[k] ]
-      @model[k] = [ 200*k, 0.0 ]
     end
-    # unpack the analysis header
+    # unpack the analysis header (multi-byte values from device are little-endian)
     binary = "\0\0\0\0\0\0\0\0\0\0\0"
     binary.length.times do |k|
       binary[k] = @dump.header[2*k,2].hex
     end
-    if (0..1).include? @dump.source then
+    case @dump.source
+    when 0,1
       # powerAnalysis
       keys = [:nClipped,:currentComplexity,:currentRMS,:currentPhase,:relativePhase]
       values = binary.unpack("CCevv")
@@ -37,11 +36,26 @@ class Engineering::DumpController < Engineering::ApplicationController
       amplitude = Math.sqrt(2)*@results[:currentRMS]
       tzero = @results[:currentPhase]
       mean = @dump.samples.sum/@dump.samples.length
+      @model = Array.new 250
       250.times do |k|
-        t = @model[k][0]
-        @model[k][1] = mean + amplitude*Math.sin(@@omega*(t-tzero))
+        t = 200*k # microseconds
+        @model[k] = [t, mean + amplitude*Math.sin(@@omega*(t-tzero))]
       end
+    when 4
+      # phaseAnalysis
+      keys = [:moment1,:moment0,:voltagePhase]
+      values = binary.unpack("VVv")
+      @results = Hash[*keys.zip(values).flatten]
+      # the model has spikes at the fiducial signal centroids
+      @model = [ [0,0] ]
+      t = @results[:voltagePhase]
+      while t < 50000 do
+        @model << [t-1,0] << [t,1024] << [t+1,0]
+        t += @@micros_per_120Hz
+      end
+      @model << [50000,0]
     else
+      @model = [ ]
       @results = { }
     end
     respond_to do |format|
@@ -53,6 +67,7 @@ class Engineering::DumpController < Engineering::ApplicationController
 protected
 
   @@omega = 2*Math::PI*60/1e6
+  @@micros_per_120Hz = 1e6/120
 
   def dump_raw_samples
     # Dumps raw sample data suitable for gnuplot and offline analysis
