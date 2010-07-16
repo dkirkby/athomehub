145268573b4f54a0eb94a70a87fba4de5da33a4b
David Kirkby <dkirkby@uci.edu>
2010-07-16 12:24:51 -0700

    implement new action and view

--- app/controllers/engineering/profile_controller.rb
+++ app/controllers/engineering/profile_controller.rb
@@ -4,4 +4,11 @@ class Engineering::ProfileController < Engineering::ApplicationController
     @profiles = DeviceProfile.latest(@at).find(:all,:order=>'networkID ASC')
   end
 
+  def new
+    @profile = DeviceProfile.new
+    # by default, a new config displays after all existing configs
+    last = DeviceProfile.latest.last
+    @profile.display_order = last ? (last.display_order + 1) : 0
+  end
+
 end
