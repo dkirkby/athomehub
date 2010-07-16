9ae28320ece407c43442df58a866a866dacda0e2
David Kirkby <dkirkby@uci.edu>
2010-07-16 16:15:38 -0700

    remove declared association between Sample and DeviceConfig models

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,13 +1,3 @@
 class Sample < ActiveRecord::Base
 
-  belongs_to :config,
-    :class_name => "DeviceConfig",
-    :foreign_key => "networkID",
-    :primary_key => "networkID",
-    :readonly => true
-
-  def location
-    config.location if config
-  end
-
 end
