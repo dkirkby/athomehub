b6cc710a24e29279cf52e6081d36abcdf5cedb47
David Kirkby <dkirkby@uci.edu>
2010-07-17 12:49:07 -0700

    fix up handling of local config overrides

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -4,4 +4,4 @@ ATHOME = YAML.load_file("#{RAILS_ROOT}/config/athome_defaults.yml")[RAILS_ENV]
 
 # Apply local modifications if any are provided
 local = "#{RAILS_ROOT}/config/athome_local.yml"
-ATHOME.merge! YAML.load_file(local) if FileTest.exists? local
+ATHOME.merge! YAML.load_file(local)[RAILS_ENV] if FileTest.exists? local
