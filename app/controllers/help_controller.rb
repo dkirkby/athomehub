e96bf8e750234e2342da6afeef32b24bb24b7cd8
David Kirkby <dkirkby@uci.edu>
2010-07-19 10:19:14 -0700

    add help/units content

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,9 +1,6 @@
 class HelpController < ApplicationController
 
-  def home
-  end
-  
-  def trouble
-  end
+  # There is no need to create stub action methods here as long as a
+  # corresponding view has been created
 
 end
