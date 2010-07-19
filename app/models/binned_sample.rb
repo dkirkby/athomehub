c472460f213f6dd7cffadace37eaa7b37d934116
David Kirkby <dkirkby@uci.edu>
2010-07-18 22:44:43 -0700

    add some more bin code logic

--- app/models/binned_sample.rb
+++ app/models/binned_sample.rb
@@ -1,19 +1,40 @@
 class BinnedSample < ActiveRecord::Base
 
   # window size by zoom level
-  @@window_size = [ 2.minutes, 10.minutes, 1.hour, 6.hours, 1.day, 1.week, 4.weeks, 16.weeks ]
+  @@window_size = [
+    2.minutes, 10.minutes, 1.hour, 6.hours, 1.day, 1.week, 4.weeks, 16.weeks ]
 
-  # bin size by zoom level
-  @@bin_size = [ 10.seconds, 30.seconds, 3.minutes, 15.minutes, 1.hour, 6.hours, 1.day, 1.week ]
+  # bin size by zoom level: must divide evenly into half of the window size
+  # and the bin size of the next zoom level
+  @@bin_size = [
+    10.seconds, 30.seconds, 3.minutes, 15.minutes, 1.hour, 6.hours, 1.day, 1.week ]
 
-  # epoch for calculating bin indices
-  @@epoch = Time.utc(2010).to_i
+  # number of bins per half window by zoom level
+  @@bins_per_half_window = @@window_size.zip(@@bin_size).map {|wb| wb[0]/(2*wb[1]) }
 
-  def self.code(at,zoom_level)
-    # calculates the bin code corresponding to the specified time
-    raise 'Zoom level must be 0-7' unless (0..7) === zoom_level.to_i
-    index = (at.utc.to_i - @@epoch)/@@bin_size[zoom_level.to_i]
-    return (zoom_level << 28) | index
+  # epoch for calculating bin indices: use local time so that bin edges
+  # have natural alignment in the local time zone
+  @@epoch = Time.local(2010).to_i
+
+  def self.bin(at,zoom_level)
+    # Returns the bin code corresponding to the specified time.
+    raise 'Zoom level must be 0-7' unless (0..7) === zoom_level
+    bin_index = (at.utc.to_i - @@epoch)/@@bin_size[zoom_level]
+    return (zoom_level << 28) | bin_index
+  end
+
+  def self.window(window_index,zoom_level)
+    # Returns the bin code corresponding to the first bin of the indexed window.
+    raise 'Zoom level must be 0-7' unless (0..7) === zoom_level
+    # Successive windows overlap by 50%.
+    return (zoom_level << 28) | window_index*@@bins_per_half_window[zoom_level]
+  end
+  
+  def self.time(code)
+    # Returns the UTC time corresponding to the specified bin code
+    zoom_level = (code >> 28)
+    bin_index = (code & 0x0fffffff)
+    Time.at(@@epoch + bin_index*@@bin_size[zoom_level]).utc
   end
 
 end
