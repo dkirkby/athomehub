ebcf731548f4eeda69b891fdeb483abcd508c27b
David Kirkby <dkirkby@uci.edu>
2010-08-08 18:00:02 -0700

    individual plots can be toggled on/off via jquery

--- public/javascripts/athome.js
+++ public/javascripts/athome.js
@@ -91,7 +91,10 @@ function displayPlots() {
     // render the plot using the flot library
     $.plot($(this),plotData[this.id],plotOptions[this.id]);
     // display a title below the plot
-    $(this).siblings('.title').html(plotTitles[this.id]);
+    $(this).siblings('.title').html(plotTitles[this.id]).click(function() {
+      $(this).next().slideToggle('slow');
+      return false;
+    });
     // bind a hover event handler
     $(this).bind("plothover", function (event, pos, item) {
       if(item) {
