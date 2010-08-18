fa61160d98a64cd76f442a13f258549d09195638
David Kirkby <dkirkby@uci.edu>
2010-08-18 08:51:24 -0400

    home view lists devices by serial num with link to edit config

--- app/controllers/engineering/home_controller.rb
+++ app/controllers/engineering/home_controller.rb
@@ -1,6 +1,11 @@
 class Engineering::HomeController < Engineering::ApplicationController
 
   def index
+    @devices = [ ]
+    # find the latest device configurations
+    DeviceConfig.latest.find(:all,:order=>'serialNumber ASC').each do |config|
+      @devices << { :config => config }
+    end
   end
 
 end
