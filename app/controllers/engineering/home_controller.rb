7e509c6fe7f491628f892b4eb3a246da0295a896
David Kirkby <dkirkby@uci.edu>
2010-08-18 10:36:44 -0400

    add profile columns

--- app/controllers/engineering/home_controller.rb
+++ app/controllers/engineering/home_controller.rb
@@ -5,7 +5,9 @@ class Engineering::HomeController < Engineering::ApplicationController
     # find the latest device configurations
     DeviceConfig.latest(@at).find(:all,:order=>'serialNumber ASC').each do |config|
       # look for a profile with the same network ID
-      @devices << { :config => config }
+      netID = config.networkID
+      profile = DeviceProfile.for_networkID(netID,@at).last
+      @devices << { :config => config, :profile => profile }
     end
   end
 
