71616cbb87afb2fec5b0c09622c807e1914a0789
David Kirkby <dkirkby@uci.edu>
2010-08-22 07:38:28 -0400

    define shared named scopes "recent" and "bydate"

--- lib/scoped.rb
+++ lib/scoped.rb
@@ -1,10 +1,11 @@
-# A mix-in module for database models with networkID and created_at fields
+# A mix-in module for read-only database models with networkID and created_at fields
 
 module Scoped
   
   def self.included(base)
     # define some named scopes in the appropriate class context when we are included
     base.class_eval do
+
       named_scope :for_networkID, lambda { |*args|
         {
           :conditions => (args.length > 1 && Time.now - args.last > 1.second) ?
@@ -14,6 +15,22 @@ module Scoped
           :readonly => true
         }
       }
+      
+      named_scope :recent, lambda { |n|
+        {
+          :limit => n,
+          :order => 'id DESC',
+          :readonly => true
+        }
+      }
+      
+      named_scope :bydate, lambda { |begin_at,end_at|
+        {
+          :conditions => ['created_at > ? and created_at <= ?',begin_at,end_at],
+          :readonly => true
+        }
+      }
+      
     end
   end
 
