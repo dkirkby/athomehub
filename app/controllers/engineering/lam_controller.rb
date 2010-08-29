50c3f542a051fbf7a657c0563a339f5f6f6cf4c8
David Kirkby <dkirkby@uci.edu>
2010-08-29 09:50:15 -0700

    add optional nid for lam recent/bydate actions

--- app/controllers/engineering/lam_controller.rb
+++ app/controllers/engineering/lam_controller.rb
@@ -2,14 +2,23 @@ class Engineering::LamController < Engineering::ApplicationController
   
   before_filter :valid_n,:only=>:recent
   before_filter :valid_ival,:only=>:bydate
+  before_filter :optional_nid, :only=>[:recent,:bydate]
   
   def recent
     @count = LookAtMe.count
-    @lams = LookAtMe.recent(@n)
+    if @config then
+      @lams = LookAtMe.for_serialNumber(@config.serialNumber).recent(@n)
+    else
+      @lams = LookAtMe.recent(@n)
+    end
   end
 
   def bydate
-    @lams = LookAtMe.bydate(@begin_at,@end_at)
+    if @config then
+      @lams = LookAtMe.for_serialNumber(@config.serialNumber).bydate(@begin_at,@end_at)
+    else
+      @lams = LookAtMe.bydate(@begin_at,@end_at)
+    end
   end
 
   def index
