10e47dcebc3f2e1d1fceb39429ba92fbf691b1ce
David Kirkby <dkirkby@uci.edu>
2010-08-29 19:47:14 -0700

    fix bug in engineering/home profile links

--- app/controllers/engineering/home_controller.rb
+++ app/controllers/engineering/home_controller.rb
@@ -9,7 +9,10 @@ class Engineering::HomeController < Engineering::ApplicationController
       # look for a profile with the same network ID
       netID = config.networkID
       profile = DeviceProfile.for_networkID(netID,@at).last
-      dev[:profile] = profile.description if profile
+      if profile then
+        dev[:profile] = profile.description
+        dev[:profile_id] = profile.id
+      end
       # look for the most recent log of this device being configured
       last = DeviceLog.for_networkID(netID,@at).find(:last,:conditions=>'code in (-4,-5)')
       dev[:last_config] = last.created_at if last
